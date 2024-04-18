3c4c5c25433d0979909e1fee3e9f28a65f301f55
pra_jodh_gath <72610816+prajodh@users.noreply.github.com>
2024-04-18 16:16:58 -0400

    Add files via upload

--- resource-group.tf
+++ resource-group.tf
@@ -1,6 +1,6 @@
 # Creates a resource group in our Azure account.
 
-resource "azurerm_resource_group" "practicum15" {
-  name     = "practicum15"
-  location = "eastus"
+resource "azurerm_resource_group" "practicum16" {
+  name     = var.app_name
+  location = var.location
 }
